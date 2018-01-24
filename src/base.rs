66461f2d4d66e95ab4b9e44e11e85903fe4c667f
Jason Hansel <jason@jasonhansel.com>
2018-01-24 18:33:34 -0500

    Broken

--- src/base.rs
+++ src/base.rs
@@ -1,9 +1,9 @@
 use scope::*;
 use value::*;
-use expand::*;
 use value::Value::*;
 use std::rc::Rc;
 use std::borrow::Cow;
+use scope::NativeResult::*;
 
 fn get_args<'s>(args: Vec<Value<'s>>) -> (Option<Value>,Option<Value>,Option<Value>,Option<Value>,Option<Value>,Option<Value>,Option<Value>) {
     let mut it = args.into_iter();
@@ -18,30 +18,37 @@ fn get_args<'s>(args: Vec<Value<'s>>) -> (Option<Value>,Option<Value>,Option<Val
     )
 }
 
-fn change_char<'s>(args: Vec<Value<'s>>) -> Value<'s> {
+fn change_char<'s>(args: Vec<Value<'s>>) -> NativeResult<'s> {
     match get_args(args) {
-        (Some(Str(n)), Some(Str(replacement)), Some(Closure(ValueClosure(inner_scope, h))), None, ..) => {
+        (Some(Str(n)), Some(Str(replacement)), Some(Closure(ValueClosure(inner_scope, mut h))), None, ..) => {
             let needle = n.chars().next().unwrap();
-            let (mut rest, prefix) = h.split_at(true, false, &mut |ch| {
+            let mut rest = (*h).make_static();
+            let prefix = rest.split_at(true, false, &mut |ch| {
                 ch == needle
             });
             rest.split_char(); // take the matched character out
-            let new_closure = ValueClosure(inner_scope.clone(),
-                Box::new( prefix.unwrap().concat(
-                        Rope::from_str(replacement)
-                ).concat(rest) )
-            );
-            ((Value::Bubble(new_closure)))
+
+            Continue(
+                Rope::new(),
+                inner_scope,
+                prefix.unwrap().concat(
+                        Rope::from_slice(replacement)
+                ).concat(rest)
+            )
         },
         _ => { panic!() }
     }
 }
 
-fn if_eq<'s>(args: Vec<Value<'s>>) -> Value<'s> {
+fn if_eq<'s>(args: Vec<Value<'s>>) -> NativeResult<'s> {
     match get_args(args) {
         (Some(value_a), Some(value_b), Some(Closure(if_true)), Some(Closure(if_false)), None, ..) => {
-            let todo = if value_a == value_b { if_true } else { if_false };
-            ((Bubble(todo.force_clone()))) 
+            let mut todo = if value_a == value_b { if_true } else { if_false };
+            Continue(
+                Rope::new(),
+                todo.0,
+                todo.1.make_static()
+            )
         },
         _ => {panic!()}
     }
@@ -50,40 +57,38 @@ fn if_eq<'s>(args: Vec<Value<'s>>) -> Value<'s> {
 
 // FIXME: not working yet??
 
-fn if_eq_then<'s>(args: Vec<Value<'s>>) -> Value<'s> { 
+fn if_eq_then<'s>(args: Vec<Value<'s>>) -> NativeResult<'s> { 
     match get_args(args) {
-        (Some(value_a), Some(value_b), Some(Closure(if_true)), Some(Closure(if_false)), Some(Closure(finally)), None,..) => {
+        (Some(value_a), Some(value_b), Some(Closure(if_true)), Some(Closure(if_false)), Some(Closure(mut finally)), None,..) => {
             let todo = (if value_a == value_b { if_true } else { if_false }).force_clone().1;
-            Bubble(
-                ValueClosure(finally.0.clone(), Box::new(todo.concat(* finally.force_clone().1)))
-            )
+            Continue(Rope::new(), finally.0.clone(), todo.concat(* finally.force_clone().1))
         },
         _ => {panic!()}
     }
 }
 
-fn bubble<'s>(args: Vec<Value<'s>>) -> Value<'s> {
+fn bubble<'s>(args: Vec<Value<'s>>) -> NativeResult<'s> {
     match get_args(args) {
         (Some(Closure(closure)), None, ..) => {
-            ((Bubble(closure.force_clone())))
+            panic!()
         },
         _ => panic!()
     }
 }
 
-fn end_paren<'s>(args: Vec<Value<'s>>) -> Value<'s> {
+fn end_paren<'s>(args: Vec<Value<'s>>) -> NativeResult<'s> {
     match get_args(args) {
         (None, ..) => {
-            ((Value::Str(Cow::from(")".to_owned()))))
+            Done(Value::Str(ArcSlice::from_string(")".to_owned())))
         },
         _ => panic!()
     }
 }
 
-fn literal<'s>(args: Vec<Value<'s>>) -> Value<'s> {
+fn literal<'s>(args: Vec<Value<'s>>) -> NativeResult<'s> {
     match get_args(args) {
         (Some(Closure(ValueClosure(_, closure))), None, ..) => {
-            ((Value::Str( Cow::Owned( closure.to_str().unwrap().into_owned()  ))) )
+            Done(Value::Str(closure.to_str().unwrap()))
         },
         _ => { panic!() }
     }
@@ -92,20 +97,17 @@ fn literal<'s>(args: Vec<Value<'s>>) -> Value<'s> {
 // NOTE: can't define inside of parentheses (intentonally -- I think this is the only sensible
 // opton if we want to allow parallelism -- can this be changed?)
 
-fn define<'s>(args: Vec<Value<'s>>) -> Value<'s> {
+fn define<'s>(args: Vec<Value<'s>>) -> NativeResult<'s> {
     match get_args(args) {
         (Some(Str(name_args)),
         Some(Closure(ValueClosure(scope, closure_data))),
        Some(Closure(ValueClosure(_, to_expand))), None, ..) => {
 
-            if name_args.is_empty() {
-                panic!("Empty define");
-            }
             // TODO: custom arguments, more tests
             let mut parts = vec![];
             let mut params = vec![];
             let na_str = name_args;
-            for part in na_str.split(' ') {
+            for part in na_str.to_str().split(' ') {
                 if part.starts_with(':') {
                     parts.push(Param);
                     params.push((&part[1..]).to_owned());
@@ -116,8 +118,9 @@ fn define<'s>(args: Vec<Value<'s>>) -> Value<'s> {
             // make_mut clones as nec.
             let mut new_scope = Arc::new(dup_scope(&scope));
             Scope::add_user(&mut new_scope, parts, params, *closure_data);
+            println!("DEFINE");
             // TODO avoid recursion
-            new_expand(new_scope, *to_expand)
+            Continue(Rope::new(), new_scope, *to_expand)
         },
         args => {
             panic!("Invalid state {:?}", args);
@@ -126,20 +129,20 @@ fn define<'s>(args: Vec<Value<'s>>) -> Value<'s> {
     }
 }
 
-fn expand<'s>(args: Vec<Value<'s>>) -> Value<'s> {
+fn expand<'s>(args: Vec<Value<'s>>) -> NativeResult<'s> {
     match get_args(args) {
         (Some(Closure(ValueClosure(scope, contents))), None, ..) => {
-            new_expand(scope.clone(), *contents )
+            Continue(Rope::new(), scope.clone(), *contents )
         },
         _ => {panic!("ARG"); }
     }
 }
 
-fn rescope<'s>(args: Vec<Value<'s>>) -> Value<'s> {
+fn rescope<'s>(args: Vec<Value<'s>>) -> NativeResult<'s> {
     match get_args(args) {
     (Some(Closure(ValueClosure(inner_scope, _))),
     Some(Closure(ValueClosure(_, contents))),None,..) => {
-         Closure(ValueClosure(inner_scope.clone(), contents ))
+         Done(Closure(ValueClosure(inner_scope.clone(), contents )))
     },
     _ => {panic!() }
     }
